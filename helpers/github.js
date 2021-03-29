2a888715e0317fab16489835fb66eb43de2faa26
Devon Poston <dev.poston@gmail.com>
2021-03-29 15:28:26 -0700

    successful axios request to github api, completed helper module getreposbyusername

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,28 @@
 const axios = require('axios');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (user, callback) => {
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    method: 'get',
+    url: `https://api.github.com/users/${user}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
-
+  axios(options)
+    .then((response) => {
+      console.log('SUCCESSFULL AXIOS GET REQUEST: ', response);
+      callback(response);
+    })
+    .catch((error) => {
+      console.log('FAILED AXIOS GET REQUEST: ', error);
+    });
 }
 
 module.exports.getReposByUsername = getReposByUsername;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const getRepos = require('../helpers/github.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 
 app.post('/repos', function (req, res) {
-  console.log('POST REQUEST RECEIVED @ SERVER: ', req.body);
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  console.log('POST REQUEST RECEIVED @ SERVER: ', req.body.owner);
+  getRepos.getReposByUsername(req.body.owner, (response) => {
+    console.log('SUCCESSFUL AXIOS GET REQUEST @ SERVER: ', response.data);
+  })
 });
 
 app.get('/repos', function (req, res) {
