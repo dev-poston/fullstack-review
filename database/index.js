2f770314dace9c52a571cd8a730bf71d22b005fb
Devon Poston <dev.poston@gmail.com>
2021-04-01 13:49:34 -0700

    mdb errors

--- database/index.js
+++ database/index.js
@@ -47,11 +47,11 @@ let find = (user, callback) => {
     .then((data) => {
       console.log('MONOGODB - SEARCHING DB');
       callback(data);
+    })
+    .catch((error) => {
+      console.log('MONOGODB - SEARCH FAILED');
+      callback(error);
     });
-    // .catch((error) => {
-    //   console.log('MONOGODB - SEARCH FAILED');
-    //   callback(error);
-    // })
 };
 
 let update = (user, doc, callback) => {
