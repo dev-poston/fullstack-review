8a7e739e50c8056aca2a5fbeb52f58746ec33017
Devon Poston <dev.poston@gmail.com>
2021-04-01 01:27:19 -0700

    endpoint probs

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-const serverURL = 'https://afternoon-atoll-48900.herokuapp.com/server';
+const serverURL = 'https://afternoon-atoll-48900.herokuapp.com/';
 
 app.post(serverURL, function (req, res) {
   db.find(req.body, (cursor) => {
