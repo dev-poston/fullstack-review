6794e62eed2eb6487c3d51dbb4dce8d3ef4a7494
Devon Poston <dev.poston@gmail.com>
2021-04-01 20:16:01 -0700

    fingers crossed

--- database/index.js
+++ database/index.js
@@ -4,17 +4,17 @@ const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost/fetcher');
 //================================================
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useMongoClient: true
-// });
-// let db = mongoose.connection;
+mongoose.connect(process.env.MONGODB_URI, {
+  useMongoClient: true
+});
+let db = mongoose.connection;
 
-// db.once('open', () => {
-//   console.log('CONNECTED TO MongoDB!');
-// });
-// db.on('error', (error) => {
-//   console.log('FAILED TO CONNECT TO MONGODB:', error);
-// });
+db.once('open', () => {
+  console.log('CONNECTED TO MongoDB!');
+});
+db.on('error', (error) => {
+  console.log('FAILED TO CONNECT TO MONGODB:', error);
+});
 //=============================================================================
 // const MongoClient = require('mongodb').MongoClient;
 // const assert = require('assert');
@@ -29,13 +29,13 @@ const mongoose = require('mongoose');
 //   client.close();
 // });
 //======================================================================
-var MongoClient = require('mongodb').MongoClient;
-var uri = process.env.MONGODB_URI;
-MongoClient.connect(uri, function(err, client) {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// var MongoClient = require('mongodb').MongoClient;
+// var uri = process.env.MONGODB_URI;
+// MongoClient.connect(uri, function(err, client) {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 //====================================================================
 let repoSchema = mongoose.Schema({
   _id: Number,
