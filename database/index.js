09ce691115bb5b233a04067a60900f8a3169b51a
Devon Poston <dev.poston@gmail.com>
2021-04-01 17:02:54 -0700

    mdb test

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 //const config = require('../config.js');
 
-mongoose.connect('mongodb://localhost/fetcher');
+// mongoose.connect('mongodb://localhost/fetcher');
 mongoose.connect(process.env.MONGODB_URI);
 let db = mongoose.connection;
 
